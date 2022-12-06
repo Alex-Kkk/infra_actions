6b8ab9b73758dbf1e898ac087b7f7310f98e7d46
Alex K <h_xx@mail.ru>
2022-12-06 23:29:16 +1100

    Test

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -60,9 +60,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'infra_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
